522b82650f1a88e1746f7fdd9e53b237d81d1ca0
Wiseclone <40637778+Wiseclone@users.noreply.github.com>
2020-10-06 11:30:01 -0500

    Update search.js

--- js-functions/search.js
+++ js-functions/search.js
@@ -1,6 +1,8 @@
 // Configuration variables
 searchables = [["Graduates", gradSet], ["Staff", staffSet]] // Lists that can be searched. [name, array]
 sourceSet // List to search through
+textQuery = document.forms["search"]["textQuery"].value; // Search term
+queryCat = document.forms["search"]["queryCat"].value; // Category to search. Defaults to all.
 // Initialization functions
 searchContainer = document.getElementById("searchContainer");
 searchContainer.onload = function(){
